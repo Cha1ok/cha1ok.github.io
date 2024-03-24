2e0cb01e2d9acbc8e1785afc78befddc94887477
Cha1ok <128479736+Cha1ok@users.noreply.github.com>
2024-03-24 15:23:26 +0300

    Update form.php

--- home3/form.php
+++ home3/form.php
@@ -1,51 +1,45 @@
 <form class="popup" action="" method="post">
         <label>
-            <input type="text" name="name">
+            <input type="text" name="name" class="input">
             <div class="label__text">
                 Ваше имя
             </div>
         </label>
         <label>
-            <input type="tel" name="tel">
+            <input type="tel" name="number" class="input">
             <div class="label__text">
                 Ваш телефон
             </div>
         </label>
         <label>
-            <textarea name="message"></textarea>
+            <textarea name="biography" class="input"></textarea>
             <div class="label__text">
-                Ваше сообщение
+                биография
             </div>
         </label>
         <label>
-            <input type="email" name="email">
+            <input type="email" name="email" class="input">
             <div class="label__text">
                 Ваша поча
             </div>
         </label>
         <label>
-            <input type="text" name="organization">
-            <div class="label__text">
-                Ваша организация
-            </div>
-        </label>
-        <label>
-            <input type="date" name="data">
+            <input type="date" name="data" class="input">
             <div class="'label__text">
                 Ваша дата
             </div>
         </label>
         <label>
             <label><input type="radio" checked="checked"
-                name="radio-group-1" value="Значение1" />
+                name="radio" value="Значение1" />
                 Мужской</label>
               <label><input type="radio"
-                name="radio-group-1" value="Значение2" />
+                name="radio" value="Значение2" />
                 Женский</label>
             <div class="label__text">ваш пол</div>
         </label>
         <label>
-            <select name="programm">
+            <select name="lang[]" id="lang" multiple="multiple">
                 <option value="Значение1">Java</option>
                 <option value="Значение2" selected="selected">C++</option>
                 <option value="Значение3" selected="selected">HTML</option>
@@ -55,8 +49,8 @@
             <div class="label__text">Любимый язык програмирования</div>
         </label>
         <label>
-            <input type="checkbox" required
-            name="check-1" /> 
+            <input type="checkbox"
+            name="check_mark" id="oznakomlen" /> 
             <div class="label__text">
                 Ознакомлен
             </div>
